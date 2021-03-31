dfc1dc9b839f994d21647eed2eb313c0e35aa40b
didadadida93 <didadadida93@gmail.com>
2021-04-01 06:58:14 +0700

    Setup development environment
    * add `dotenv` package, the savior when developing app
    * add `nodemon` package for hot reload when something change, speed
      up development process

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const fs = require('fs')
 const http2 = require('http2')
 const app = require('./src/app')
 
-const port = process.env.PORT || 443
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config()
+}
+
+const port = process.env.PORT
 const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/autochess-vng-api.didadadida93.xyz/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/autochess-vng-api.didadadida93.xyz/fullchain.pem"),
+  key: fs.readFileSync(process.env.PRIVATE_KEY_DIRECTORY),
+  cert: fs.readFileSync(process.env.CERTIFICATE_DIRECTORY),
   allowHTTP1: true
 }
 
 http2.createSecureServer(options, app.callback())
-  .listen(port, function() {
-    console.log('listening on port', port)
-  })
+  .listen(port, () => console.log('listening on port', port))
