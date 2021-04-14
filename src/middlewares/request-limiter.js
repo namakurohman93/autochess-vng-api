058d85ba7330057083597e24df1ff255dc7dda7d
didadadida93 <didadadida93@gmail.com>
2021-04-14 13:42:31 +0700

    add custom header on response header
    * add `RateLimit-Remainig` to the response header so the client can
      calculate how much request they can sent
    * add `RateLimit-Reset` to the response header so the client can
      calculate when the rate limit reset happen
    
    when the client reach their rate limit, the response will contain
    custom header `RateLimit-Reset` so the client can calculate when
    the rate limit will be reset.
    
    please note that the time is set to UTC + 0.

--- src/middlewares/request-limiter.js
+++ src/middlewares/request-limiter.js
@@ -1,8 +1,24 @@
 module.exports = limiter => async (ctx, next) => {
+  let resetTime
+
   try {
-    await limiter.consume(ctx.ip)
+    let rateLimiterRes = await limiter.consume(ctx.ip)
+    resetTime = new Date(Date.now() + rateLimiterRes.msBeforeNext)
+
+    ctx.set('RateLimit-Remaining', rateLimiterRes.remainingPoints)
+    ctx.set('RateLimit-Reset', resetTime.toUTCString())
+
     await next()
   } catch(e) {
+    if (e instanceof Error) {
+      ctx.status = 500
+      ctx.body = { error: true, message: 'internal server error' }
+      return
+    }
+
+    resetTime = new Date(Date.now() + e.msBeforeNext)
+
+    ctx.set('RateLimit-Reset', resetTime.toUTCString())
     ctx.status = 429
     ctx.body = { error: true, message: 'too many requests' }
   }
