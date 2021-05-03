9605cd4cac25f1e0e62ea125ab2351e72aca62ce
didadadida93 <didadadida93@gmail.com>
2021-05-03 16:31:59 +0700

    Simplify change of data types
    I finally find out on how to simplify changing data types.
    So instead using this code
    
    ```
    const limit_points: string = process.env.RATE_LIMITER_POINTS as string
    const limit_duration: string = process.env.RATE_LIMITER_DURATION as string
    
    const points: number = +limit_points
    const duration: number = +limit_duration
    ```
    
    I changed it into
    
    ```
    const points: number = Number(process.env.RATE_LIMITER_POINTS as string)
    const duration: number = Number(process.env.RATE_LIMITER_DURATION as string)
    ```

--- src/rate-limiter.ts
+++ src/rate-limiter.ts
@@ -4,11 +4,8 @@ import { RateLimiterRedis, RateLimiterMemory } from 'rate-limiter-flexible'
 export type RateLimiter = RateLimiterRedis | RateLimiterMemory
 
 export default function() {
-  const limit_points: string = process.env.RATE_LIMITER_POINTS as string
-  const limit_duration: string = process.env.RATE_LIMITER_DURATION as string
-
-  const points: number = +limit_points
-  const duration: number = +limit_duration
+  const points: number = Number(process.env.RATE_LIMITER_POINTS as string)
+  const duration: number = Number(process.env.RATE_LIMITER_DURATION as string)
 
   if (process.env.NODE_ENV == 'test') {
     return new RateLimiterMemory({
