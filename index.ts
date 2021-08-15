9953ebb992cf281e65ba6bc0b69ab6dd18f1b0df
didadadida93 <didadadida93@gmail.com>
2021-08-15 08:13:45 +0700

    simplier app

--- index.ts
+++ index.ts
@@ -5,35 +5,16 @@ import http2 from 'http2'
 import { config } from 'dotenv'
 import app from './src/app'
 
-interface Http2Option {
-  key: Buffer
-  cert: Buffer
-  allowHTTP1: boolean
-}
-
 if (process.env.NODE_ENV == 'development') {
   config()
 }
 
 const port: string = process.env.PORT as string
 const httpPort: string = process.env.HTTP_PORT as string
-const options: Http2Option = {
-  key: fs.readFileSync(process.env.PRIVATE_KEY_DIRECTORY as string),
-  cert: fs.readFileSync(process.env.CERTIFICATE_DIRECTORY as string),
-  allowHTTP1: true
-}
 
 async function main() {
   const handler: Koa = await app()
-
-  http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers.host + req.url })
-    res.end()
-  })
-    .listen(httpPort, () => console.log('running redirect all http connection'))
-
-  http2.createSecureServer(options, handler.callback())
-    .listen(port, () => console.log('listening on port', port))
+  handler.listen(httpPort, () => console.log('listening on port', httpPort))
 }
 
 main().catch(e => console.log(e))
